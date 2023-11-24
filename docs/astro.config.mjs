3a28a4c15ba489f6c214ad99def12f969f8199fa
Univa <41708691+Univa@users.noreply.github.com>
2023-11-24 14:16:31 -0500

    initial commit for user docs site

---
+++ docs/astro.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "rumcake",
+      lastUpdated: true,
+      tableOfContents: {
+        minHeadingLevel: 1,
+      },
+      social: {
+        github: "https://github.com/Univa/rumcake",
+      },
+      pagination: false,
+      sidebar: [
+        {
+          label: "Information",
+          items: [
+            {
+              label: "Introduction",
+              link: "/",
+            },
+          ],
+        },
+        {
+          label: "Getting Started",
+          autogenerate: { directory: "getting-started" },
+        },
+        {
+          label: "Features",
+          autogenerate: { directory: "features" },
+        },
+        {
+          label: "API Reference",
+          link: "https://github.com/Univa/rumcake",
+        },
+      ],
+    }),
+  ],
+});
