58d960bed02b1dcb41c33227ba38b4d682b4b8c9
Univa <41708691+Univa@users.noreply.github.com>
2023-11-25 00:47:48 -0500

    add action to deploy docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://univa.github.io",
   base: "/rumcake",
   trailingSlash: "always",
   build: {
